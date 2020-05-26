e244b7267119f2ccdd80782390fadce2d32773e0
Anton Byrko <antonbyrko@gmail.com>
2020-05-27 02:23:10 +0300

    part3 Variables tasks

--- Program.cs
+++ Program.cs
@@ -4,9 +4,72 @@ namespace ITVDN_start
 {
     class Program
     {
+      const double PI = 3.141592653;  
         static void Main(string[] args)
         {
-            Machine_Mathematics_3();
+            Variables_additional();
+        }
+
+        static void Variables_1()
+        {
+            int x = 10, y = 12, z = 3;
+            x += y - x++ * z;
+            z = --x - y * 5;
+            y /= x + 5 % z;
+            z = x++ + y * 5;
+            x = y - x++ * z;
+
+            Console.WriteLine($"x= {x}");
+            Console.WriteLine($"y= {y}");
+            Console.WriteLine($"z= {z}");
+        }
+
+        static void Variables_2()
+        {
+            int x = 5, y = 6, z = 11;
+
+            double averege =(x + y + z) / 3d;
+
+            Console.WriteLine($"averege {x} + {y} + {z} = {averege}");
+        }
+
+        static void Variables_3()
+        {
+            Console.Write("Enter radius ");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"circle area = {PI*Math.Pow(r,2)}");
+        }
+
+        static void Variables_4()
+        {
+            Console.Write("Enter radius ");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter height ");
+            double h = Convert.ToDouble(Console.ReadLine());
+
+            double volume = PI * Math.Pow(r, 2) * h;
+            double area = 2 * PI * r * (r + h);
+
+            Console.WriteLine($"cilinder voume = {volume}");
+            Console.WriteLine($"cilinder area = {area}");
+        }
+
+        static void Variables_additional()
+        {
+            Console.Write("Enter fisrs variable (a) ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second variable (b) ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(
+                $"a + b = {a + b}\n" +
+                $"a - b = {a - b}\n" +
+                $"a * b = {a * b}\n" +
+                $"a / b = {(double)a / b}\n" +
+                $"a % b = {a % b}");
         }
 
         static void Machine_Mathematics_2()
@@ -24,5 +87,6 @@ namespace ITVDN_start
             Console.WriteLine(s2);
             Console.WriteLine(s3);
         }
+
     }
 }
